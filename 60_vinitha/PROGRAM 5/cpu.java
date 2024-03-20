6dbba13cbe830fcc647cfd6f2a0ae6f569a7f8b2
vinithapammu <145668801+vinithapammu@users.noreply.github.com>
2024-03-20 15:31:37 +0530

    Add files via upload

---
+++ 60_vinitha/PROGRAM 5/cpu.java
@@ -0,0 +1,36 @@
+package project1;
+
+public class cpu {
+	double price;
+	class processor{
+		double cores;
+		String manufacturer;
+		double getcache() {
+			return 4.3;
+			
+		}
+	}
+	static class RAM
+	{
+		double memory;
+		String manufacturer;
+		double getClockSpeed() {
+			return 5.5;
+		}
+	}
+
+public class CpuDetails
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		cpu c1=new cpu();
+		cpu.processor p1=c1.new processor();
+		cpu.RAM ram=new cpu.RAM();
+		System.out.println("Processor cache= "+p1.getcache());
+		System.out.println("RAM clock speed="+ram.getClockSpeed());
+	}
+}
+	
+}
+
+
