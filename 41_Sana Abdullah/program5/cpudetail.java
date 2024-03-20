96d4a69cd53e635777a57548ee570cd5aaa81814
sanaabdulla <145667490+sanaabdulla@users.noreply.github.com>
2024-03-20 15:31:19 +0530

    Add files via upload

---
+++ 41_Sana Abdullah/program5/cpudetail.java
@@ -0,0 +1,25 @@
+package cpudetail;
+
+class CPU {
+	double price;
+	public class Processor {
+		double cores;
+		String manufacturer;
+		double getCache () {
+			return 4.3;}}
+	static class RAM{
+		double memory;
+		String manufacturer;
+		double getClockSpeed() 
+		{
+			return 5.5 ;}
+		}}
+public class cpudetail {
+	public static void main(String[] args) {
+		CPU cpu = new CPU(); 
+		CPU.Processor processor = cpu.new Processor();
+		CPU.RAM ram = new CPU.RAM();
+		System.out.println("Processor Cache =" + processor.getCache());
+		System.out.println("RAM Clock speed = "+ram.getClockSpeed());
+	}
+}
