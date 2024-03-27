1b2e11623cf1357da3b1733a78da0020073fb028
bhavanakbabu <146074730+bhavanakbabu@users.noreply.github.com>
2024-03-27 15:07:11 +0530

    Add files via upload

---
+++ 10_Bhavana_k_Babu/05-CPU details/cpudetails/src/cpu/cpu.java
@@ -0,0 +1,40 @@
+package cpu;
+import java.util.Scanner;
+public class cpu {
+	static Scanner sc=new Scanner(System.in);
+	double price;
+	class Processor{
+		double cores;
+		String manufacturer;
+		double getCache() {
+			
+			System.out.println("enter the Cache");
+			double cache = sc.nextDouble();
+			return cache;
+			
+		}
+	}
+	static class RAM{
+		double memory ;
+		String manufacturer;
+		double getClockSpeed() {
+			System.out.println("enter the Clock speed");
+			double clk = sc.nextDouble();
+			return clk;
+		}
+		
+	}
+	
+}
+
+class CPDetails {
+	public static void main(String[] args) {
+		cpu c = new cpu();
+		cpu.Processor p = c.new Processor();
+		cpu.RAM ram = new cpu.RAM();
+		double z =p.getCache();
+		double x = ram.getClockSpeed();
+		System.out.println("Procesor cache = "+ z);
+		System.out.println("Ram clock speed = "+ x);
+	}
+}
