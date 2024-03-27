9c5407f232e52b2359c266cc5561d48773ac8fb4
sahalasahla <145668144+sahalasahla@users.noreply.github.com>
2024-03-27 09:40:52 +0000

    Update Cpu.java

--- 40_Sahala_Tk/05-Cpu/Cpu.java
+++ 40_Sahala_Tk/05-Cpu/Cpu.java
@@ -1,27 +1,40 @@
-
-public class Cpu {
+package cpu;
+import java.util.Scanner;
+public class cpu {
+	static Scanner sc=new Scanner(System.in);
 	double price;
 	class Processor{
 		double cores;
 		String manufacturer;
-		static double getcache() {
-			return 4.3;
+		double getCache() {
+			
+			System.out.println("enter the Cache");
+			double cache = sc.nextDouble();
+			return cache;
+			
 		}
 	}
 	static class RAM{
-		double memory;
+		double memory ;
 		String manufacturer;
-		double getClockspeed() {
-			return 5.5;
+		double getClockSpeed() {
+			System.out.println("enter the Clock speed");
+			double clk = sc.nextDouble();
+			return clk;
 		}
+		
 	}
-
+	
 }
-class CpuDetails{
-	public static void main(String[]args) {
-		Cpu cpu=new Cpu();
-	Cpu.Processor processor=cpu.new Processor();
-		Cpu.RAM ram=new Cpu.RAM();
-		System.out.println("processor cache="+ Cpu.Processor.getcache());
-		Sytem.out.println(Ram clock speed
+
+class CPD {
+	public static void main(String[] args) {
+		cpu c = new cpu();
+		cpu.Processor p = c.new Processor();
+		cpu.RAM ram = new cpu.RAM();
+		double z =p.getCache();
+		double x = ram.getClockSpeed();
+		System.out.println("Procesor cache = "+ z);
+		System.out.println("Ram clock speed = "+ x);
+	}
 }
