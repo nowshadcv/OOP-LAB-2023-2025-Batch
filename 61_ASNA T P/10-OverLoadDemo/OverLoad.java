dc3abe1f65c148bdd5d2d2e9653cc2b032239704
Asna587 <118503675+Asna587@users.noreply.github.com>
2024-03-27 15:17:10 +0530

    Add files via upload

---
+++ 61_ASNA T P/10-OverLoadDemo/OverLoad.java
@@ -0,0 +1,13 @@
+package PackA;
+ class OverLoad {
+
+	public static void main(String[] args) {
+		OverLoadDemo ob = new OverLoadDemo();
+		ob.area(5);
+		ob.area(11,12);
+		ob.area(2.5); }} //end
+		
+
+	
+
+
