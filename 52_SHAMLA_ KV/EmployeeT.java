aa98e9ec85a238d8b827597d86278848eb8806aa
shamlakv <146522964+shamlakv@users.noreply.github.com>
2024-04-03 10:56:17 +0100

    Update EmployeeT.java

--- 52_SHAMLA_ KV/EmployeeT.java
+++ 52_SHAMLA_ KV/EmployeeT.java
@@ -1,7 +1,9 @@
 package program;
 
+import java.util.Scanner;
+
 public class EmployeeT {
- int empid;
+			int empid;
 			String name;
 			float salary;
 			String address;
@@ -23,7 +25,39 @@ public class EmployeeT {
 		System.out.println("Teacher salary:"+ salary);
 		System.out.println("Teacher address:"+address);
 		System.out.println("Teacher department:"+ department);
-		System.out.println("Teacher subject:"+subject);}}
-		
+		System.out.println("Teacher subject:"+subject);}
+	public class TeacherArrObjects {
+		public static void main(String[] args) {
+			System.out.println("enter the number of teachers : ");
+			Scanner sc = new Scanner(System.in);
+			int n=sc.nextInt();
+			System.out.println("enter teachers details one by one.. : ");
+			Teacher teacher []=new Teacher [n];
+			Scanner scT=new Scanner(System.in);
+			int tid;String name;float salary;String address;
+			String department;String subject;
+			for(int i=0; i<n;i++) {
+				System.out.println("enter" +i+ " teacher details.. : ");
+				System.out.println("enter  teacher id(integer) : ");
+				tid=scT.nextInt();
+				System.out.println("enter  teacher name(string) : ");
+				name=scT.next();
+				System.out.println("enter  teacher salary(float) : ");
+				salary=scT.nextFloat();
+				System.out.println("enter  teacher address(string) : ");
+				address=scT.next();
+				System.out.println("enter  teacher department(string) : ");
+				department=scT.next();
+				System.out.println("enter  teacher subject(string) : ");
+				subject=scT.next();
+				Teacher t=new Teacher(tid,name,salary,address,department,subject);
+				teacher[i]=t;}
+			System.out.println("teachers are:\n");
+			for(Teacher x:teacher) {
+				x.display();System.out.println("\n");	
 			
+		}
+	}
+
 }
+	}
