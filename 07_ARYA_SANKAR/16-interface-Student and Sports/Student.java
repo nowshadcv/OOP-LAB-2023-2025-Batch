f7dc59e2b823326fe952d01f7298fe984caa373a
aryaanadam55 <145668673+aryaanadam55@users.noreply.github.com>
2024-04-16 11:00:16 +0000

    Add files via upload

---
+++ 07_ARYA_SANKAR/16-interface-Student and Sports/Student.java
@@ -0,0 +1,24 @@
+package product;
+
+public interface Student {
+int score=10;
+void displayScore();}
+interface Sports{
+	int score=25;
+	void displaySportsScore();
+}
+class Result implements Student,Sports{
+	public void displayScore() {
+		System.out.println("Academic Score is :"+Student.score);
+	}
+	public void displaySportsScore() {
+		System.out.println("Sports Score is :"+Sports.score);
+	}
+}
+ class SportStudentResult{
+	public static void main(String [] args) {
+		Result r=new Result();
+		r.displayScore();
+		r.displaySportsScore();
+	}
+}
