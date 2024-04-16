70c24fa43cf585fffb7f07fd312ce302f994d5d6
jasnanufais881 <145668450+jasnanufais881@users.noreply.github.com>
2024-04-16 11:02:02 +0000

    Add files via upload

---
+++ 21_JASNA_JABIN/15-Inheritance-Publisher/Publisher.java
@@ -0,0 +1,47 @@
+package product;
+
+public class Publisher {
+	String publisher;
+	Publisher(String publi){
+		this.publisher=publi;
+	}
+}
+class Book{
+	  String name;
+	 Publisher publisher;
+	 Book(){}
+	 public Book(String name,Publisher publisher) {
+		 this.name=name;
+		 this.publisher=publisher;
+	 }
+}
+class Literature extends Book{
+	String Lit_type="Literature";
+	Literature(String name,Publisher publisher){
+		super (name,publisher);}
+			void display() {
+				System.out.println("Nmae:"+super.name);
+				System.out.println("Type:"+this.Lit_type);
+				System.out.println("Publisher:"+this.publisher.publisher);
+			}
+	}
+class Fiction extends Book{
+	String Lit_type="Fiction";
+	Fiction(String name,Publisher publisher){
+		super (name,publisher);}
+		void display() {
+			System.out.println("Nmae:"+super.name);
+			System.out.println("Type:"+this.Lit_type);
+			System.out.println("Publisher:"+this.publisher.publisher);
+		}
+}
+ class InheritanceBook{
+	public static void main(String [] args) {
+		Publisher lp=new Publisher("S.chand");
+		Literature l=new Literature("As you like it",lp);
+		l.display();
+		Publisher fp=new Publisher("Tata McGraw Hill");
+		Fiction f=new Fiction("Tempest",fp);
+		f.display();
+	}
+}
