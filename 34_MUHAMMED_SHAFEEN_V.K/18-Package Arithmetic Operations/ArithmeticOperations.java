0c517d96e8d702f68d68d6dac70e51d229d2e045
muhd-shafeen-34 <145667235+muhd-shafeen-34@users.noreply.github.com>
2024-05-10 12:04:02 +0530

    Add files via upload

---
+++ 34_MUHAMMED_SHAFEEN_V.K/18-Package Arithmetic Operations/ArithmeticOperations.java
@@ -0,0 +1,9 @@
+package Arithmetic;
+
+public interface ArithmeticOperations {
+	double add (double a,double b);
+	double subtract (double a,double b);
+	double multiply (double a,double b);
+	double division (double a,double b);
+
+}
