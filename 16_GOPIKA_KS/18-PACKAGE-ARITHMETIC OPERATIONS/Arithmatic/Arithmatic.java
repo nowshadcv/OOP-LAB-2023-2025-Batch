61336640473ddf31446a4df9a79ef2bac6eb8f46
gopikaks18 <146523805+gopikaks18@users.noreply.github.com>
2024-05-10 14:08:46 +0530

    Add files via upload

---
+++ 16_GOPIKA_KS/18-PACKAGE-ARITHMETIC OPERATIONS/Arithmatic/Arithmatic.java
@@ -0,0 +1,36 @@
+package Arithmatic;
+
+public class Arithmatic {
+	public double add(double a, double b)
+    {
+        return a + b;
+    }
+
+
+
+    public double subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+
+// MultiplicationImpl class implementing Multiplication interface
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+
+
+// DivisionImpl class implementing Division interface
+
+    public double divide(double a, double b) {
+        if (b != 0) {
+        	return a / b; 
+        }
+        else {
+        	throw new ArithmeticException("Cannot divide by zero");
+    }
+   }
+
+}
