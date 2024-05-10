ebf776995b4b27af28f9cf1306262157337265d8
farooq <145667687+farucq@users.noreply.github.com>
2024-05-10 11:25:33 +0530

    Update and rename product.java to 02-PRODUCT_CLASS.java

--- farooq/02-PRODUCT_CLASS.java
+++ farooq/02-PRODUCT_CLASS.java
@@ -40,3 +40,32 @@ public class product {
 	}
 
 }
+public class cars {
+
+	public static void main(String[] args) {
+		product p1=new product();
+		p1.pname="benz";
+		p1.pcode="car1";
+		p1.price=8000000;
+		
+		System.out.println("Displaying p1:");
+		p1.display();
+		
+		product p2=new product("BMW","car2",8900000);
+		
+		System.out.println("\nDisplaying p2:");
+		p2.display();
+		
+		product p3=new product("Audi","car3",6900000);
+		
+		System.out.println("\nDisplaying p2:");
+		p3.display();
+		
+		product p=p3.getPrice()<(p1.price<p2.price?p1.price:p2.price)?p3:(p1.price<p2.price?p1:p2);
+		
+		System.out.println("\nDisplaying product with lowest:");
+		p.display();
+		
+	}
+
+}
