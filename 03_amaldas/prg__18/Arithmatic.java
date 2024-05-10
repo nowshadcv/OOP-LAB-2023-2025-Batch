cce2ceef1035859e4c05c2b2ffe4c82eeb26fef1
amaldas14 <144892572+amaldas14@users.noreply.github.com>
2024-05-10 11:56:40 +0530

    Add files via upload

---
+++ 03_amaldas/prg__18/Arithmatic.java
@@ -0,0 +1,38 @@
+package Arithmatic;
+
+
+public class Arithmatic implements ArithmeticOperations{
+	
+	    public double add(double a, double b)
+	    {
+	        return a + b;
+	    }
+	
+	
+
+	    public double subtract(double a, double b)
+	    {
+	        return a - b;
+	    }
+
+
+	// MultiplicationImpl class implementing Multiplication interface
+	
+	    @Override
+	    public double multiply(double a, double b) {
+	        return a * b;
+	    }
+	
+
+
+	// DivisionImpl class implementing Division interface
+	
+	    public double divide(double a, double b) {
+	        if (b != 0) {
+	        	return a / b; 
+	        }
+	        else {
+	        	throw new ArithmeticException("Cannot divide by zero");
+	    }
+	   }
+}
