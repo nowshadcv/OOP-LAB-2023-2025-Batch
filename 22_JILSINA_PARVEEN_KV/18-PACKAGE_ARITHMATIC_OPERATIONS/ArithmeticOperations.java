fad2958111df23e08497abf9b1cd34642a0deac1
jilsinaparveenkv <146071330+jilsinaparveenkv@users.noreply.github.com>
2024-05-10 06:52:05 +0100

    Delete 22_JILSINA_PARVEEN_KV/18-PACKAGE_ARITHMATIC_OPERATIONS directory

--- 22_JILSINA_PARVEEN_KV/18-PACKAGE_ARITHMATIC_OPERATIONS/ArithmeticOperations.java
+++
@@ -1,9 +0,0 @@
-package Arithmatic;
-
-public interface ArithmeticOperations {
-	double add (double a,double b);
-	double subtract (double a,double b);
-	double multiply (double a,double b);
-	double division (double a,double b);
-
-}
